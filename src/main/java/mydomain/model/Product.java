c5ee324ef0741680d90201eff60934551b206f82
hopecee <hopecee@yahoo.com>
2018-01-04 19:04:00 +0100

    Update and rename Person.java to Product.java

---
+++ src/main/java/mydomain/model/Product.java
@@ -0,0 +1,37 @@
+package mydomain.model;
+
+import javax.jdo.annotations.*;
+
+@PersistenceCapable(identityType = IdentityType.APPLICATION, cacheable = "false", detachable = "true")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 8269335445554701873L;
+
+    @PrimaryKey
+    @Persistent(valueStrategy = IdGeneratorStrategy.IDENTITY)
+    long id;
+    @Persistent
+    private String name = null;
+    
+     public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" + "id=" + id + ", name=" + name + '}';
+    }
+    
+}
